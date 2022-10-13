79f7b69478a49bb19b2ad4f0f7626056388c2113
harik2 <mohamed.harik21@gmail.com>
2022-10-13 15:33:52 +0100

    les variables en php

---
+++ variables.php
@@ -0,0 +1,21 @@
+<?php 
+$age=19;
+$p="Ali";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>les variables    </title>
+</head>
+<body>
+    <h1 style="color:red"><?php echo 'prenom est  $p'; ?> a <?php echo $age; ?> ans</h1>
+   <?php echo "<h1 style='color:green'>$p a $age ans</h1>"; ?>
+   <?php echo '<h1 style="color:green">$p a $age ans</h1>'; ?>
+   <h3><?=  $p   ?> a <?=$age?> ans</h3>
+   <h3><?php echo   $p   ?> a <?php echo $age?> ans</h3>
+
+</body>
+</html>
